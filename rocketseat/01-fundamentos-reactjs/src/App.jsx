4a148be686e1eba4bc6b49ae0ac19b2fc52d6cf5
Lucas Silva <lucascdsilva.bj@gmail.com>
2024-09-05 16:00:09 -0300

    Componente Sidebar

--- rocketseat/01-fundamentos-reactjs/src/App.jsx
+++ rocketseat/01-fundamentos-reactjs/src/App.jsx
@@ -1,19 +1,25 @@
 import {Post} from "./Post.jsx";
 import './global.css'
+import styles from './App.module.css'
 import {Header} from "./components/Header.jsx";
+import {Sidebar} from "./components/Sidebar.jsx";
 
 export function App() {
     return (
         <div>
             <Header/>
-            <Post
-                author="Lucas Cavalcante"
-                content="Lorem impifdsafhi eordksf oieqwe sdasd"
-            />
-            <Post
-                author="Mariana Leonel"
-                content="Um novo post muito legal."
-            />
+            <div className={styles.wrapper}>
+                <Sidebar />
+                <main>
+                    <Post
+                        author="Lucas Cavalcante"
+                        content="Lorem impifdsafhi eordksf oieqwe sdasd"
+                    />
+                    <Post
+                        author="Mariana Leonel"
+                        content="Um novo post muito legal."
+                    /></main>
+            </div>
         </div>
     )
 }
