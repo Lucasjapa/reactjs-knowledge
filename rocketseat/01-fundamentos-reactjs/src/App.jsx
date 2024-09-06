92357a36f05c4d4c0277652536439a5b61bed838
Lucas Silva <lucascdsilva.bj@gmail.com>
2024-09-06 20:25:52 -0300

    Automatizando o Post e Configurando as propriedades de data

--- rocketseat/01-fundamentos-reactjs/src/App.jsx
+++ rocketseat/01-fundamentos-reactjs/src/App.jsx
@@ -5,24 +5,66 @@ import {Post} from "./components/Post.jsx";
 import styles from './App.module.css'
 import './global.css'
 
+// author: { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/Lucasjapa.png",
+      name: "Lucas Cavalcante",
+      role: "Software Engineer"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋',},
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀',
+      },
+      {type: 'link', content: 'jane.design/doctorcare',}
+    ],
+    publishedAt: new Date('2024-05-03 19:04:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/Lucasjapa.png",
+      name: "Cópia do Lucas Cavalcante",
+      role: "Software Engineer"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋',},
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀',
+      },
+      {type: 'link', content: 'jane.design/doctorcare',}
+    ],
+    publishedAt: new Date('2024-05-19 13:04:00')
+  },
+];
 
 export function App() {
-    return (
-        <div>
-            <Header/>
-            <div className={styles.wrapper}>
-                <Sidebar />
-                <main>
-                    <Post
-                        author="Lucas Cavalcante"
-                        content="Lorem impifdsafhi eordksf oieqwe sdasd"
-                    />
-                    <Post
-                        author="Mariana Leonel"
-                        content="Um novo post muito legal."
-                    /></main>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
 }
 
--- rocketseat/01-fundamentos-reactjs/src/components/Post.jsx
+++ rocketseat/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,52 +1,72 @@
 import styles from './Post.module.css'
 import {Comment} from "./Comment.jsx";
 import {Avatar} from "./Avatar.jsx";
+import {format, formatDistanceToNow} from "date-fns";
+import {ptBR} from "date-fns/locale";
 
-export function Post() {
-    return (
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar src="https://github.com/Lucasjapa.png"/>
+export function Post({author, publishedAt, content}) {
+  // Usando o INTL
+  // const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR', {
+  //   day: '2-digit',
+  //   month: 'long',
+  //   hour: '2-digit',
+  //   minute: '2-digit'
+  // }).format(publishedAt)
 
-                    <div className={styles.authorInfo}>
-                        <strong>Lucas Cavalcante</strong>
-                        <span>Software Engineer</span>
-                    </div>
-                </div>
+  // Usando o date-fns
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {locale: ptBR})
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    addSuffix: true,
+    locale: ptBR
+  })
 
-                <time title="05 de Setembro as 23:15" dateTime="2024-09-05 23:15:12">Publicado há 1h</time>
-            </header>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl}/>
 
-            <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da
-                    Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="">#novoprojeto </a>{' '}
-                    <a href="">#nlw</a>{' '}
-                    <a href="">#rocketseat</a>
-                </p>
-            </div>
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
 
-            <form className={styles.commentForm}>
-                <strong>Deixe seu feedback</strong>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+      </header>
 
-                <textarea
-                    placeholder="Deixe um comentário"
-                />
+      <div className={styles.content}>
+        {content.map(item =>{
+          if(item.type === 'paragraph') {
+            return <p>{item.content}</p>
+          } else {
+            return <p><a href="#">{item.content}</a></p>
+          }
+        })}
+        <p>
+          <a href="">#novoprojeto </a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
 
-                <footer>
-                    <button type="submit">Publicar</button>
-                </footer>
-            </form>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
 
-            <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
-            </div>
-        </article>
-    )
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment/>
+        <Comment/>
+        <Comment/>
+      </div>
+    </article>
+  )
 }
